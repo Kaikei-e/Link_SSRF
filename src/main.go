da8a4b77973e231c992b01aa73c610178f0e82ee
Kaikei-e <e_kaikei@protonmail.com>
2023-12-12 19:05:17 +0900

    serve static html file
    Co-authored-by: itsuki <itsuki-yamada@users.noreply.github.com>

--- src/main.go
+++ src/main.go
@@ -1,19 +1,40 @@
 package main
 
 import (
+	"html/template"
+	"io"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
 func main() {
 	e := echo.New()
 	port := ":9000"
+	t := &Template{
+		templates: template.Must(template.ParseGlob("public/*.html")),
+	}
+	e.Renderer = t
 
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+		return getHomePage(c)
 	})
 
-	e.Logger.Fatal(e.Start(port))
+	// e.POST("/", func(c echo.Context) error {
+	// 	return getHomePage(c)
+	// })
 
+	e.Logger.Fatal(e.Start(port))
+}
+
+func getHomePage(c echo.Context) error {
+	return c.Render(http.StatusOK, "hello", "hoge")
 }
