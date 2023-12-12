0f589532f91ccd341c3048aa949f3f459bb5ab5a
Kaikei-e <e_kaikei@protonmail.com>
2023-12-12 18:36:04 +0900

    Make it waiting for port 9000
    Co-authored-by: itsuki <itsuki-yamada@users.noreply.github.com>

--- src/main.go
+++ src/main.go
@@ -1,20 +1,19 @@
 package main
 
-// import (
-// 	"net/http"
+import (
+	"net/http"
 
-// 	"github.com/labstack/echo/v4"
-// )
+	"github.com/labstack/echo/v4"
+)
 
 func main() {
-	// e := echo.New()
-	// port := ":9000"
+	e := echo.New()
+	port := ":9000"
 
-	// e.Get("/", func(c echo.Context) error {
-	// 	return c.String(http.StatusOK, "Hello, World!")
-	// })
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
 
-	// e.Logger.Fatal(e.Start(port))
+	e.Logger.Fatal(e.Start(port))
 
-	println("Hello World")
 }
