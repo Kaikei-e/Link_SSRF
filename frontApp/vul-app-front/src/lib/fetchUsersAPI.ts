2dc2a2df651d4cc9a5525c1487eb6aaef7f5814f
Kaikei-e <e_kaikei@protonmail.com>
2023-12-31 00:21:33 +0900

    Remove unused code and files.

--- frontApp/vul-app-front/src/lib/fetchUsersAPI.ts
+++
@@ -1,5 +0,0 @@
-// export const fetchUsersAPI = async () => {
-//   const response = await fetch(`${import.meta.env.VITE_API_ENDPOINT}/users`);
-//   const data = await response.json();
-//   return data;
-// }
