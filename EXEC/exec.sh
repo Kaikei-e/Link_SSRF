0434d19818e3e3fca9df7e913a57c5dc4fc8fc4b
Kaikei-e <e_kaikei@protonmail.com>
2023-12-31 19:49:35 +0900

    testing patterns

--- EXEC/exec.sh
+++ EXEC/exec.sh
@@ -5,4 +5,7 @@ URL="http://localhost:8080/api/v1/register"
 HEADERS="-H 'Connection: keep-alive' -H 'Keep-Alive: timeout=5, max=100'"
 
 # pattern1
-curl -vvv -X POST $HEADERS -H "Content-Type: application/json" -d  '{"username":"aaaa", "profile_link":"http://localhost:9000/api/v1/users"}' $URL
+curl -vvv -XPOST $HEADERS -H "Content-Type: application/json" -d  '{"username":"aaaa", "profile_link":"http://localhost:8080/api/v1/users"}' $URL
+
+# pattern2
+# curl -vvv http://localhost:8080?http://localhost:9000/admin
