ae981e8ba628aec3edc34f041c93d18c4faea3d4
Kaikei-e <e_kaikei@protonmail.com>
2023-12-31 14:52:26 +0900

    Remove unnecessary database files

--- db/init/insert_users.sql
+++
@@ -1,5 +0,0 @@
-
-INSERT INTO vul_schema.users (username, profile_link) VALUES ('admin', 'https://owasp.org/');
-INSERT INTO vul_schema.users (username, profile_link) VALUES ('user1', 'https://owasp.org/');
-INSERT INTO vul_schema.users (username, profile_link) VALUES ('user2', 'https://owasp.org/');
-INSERT INTO vul_schema.users (username, profile_link) VALUES ('user3', 'https://owasp.org/');
