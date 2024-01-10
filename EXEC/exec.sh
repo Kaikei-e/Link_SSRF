790f9625732f86a56501a981cdb7f70d0a5d87aa
Kaikei-e <e_kaikei@protonmail.com>
2024-01-10 22:31:09 +0900

    Update profile link in exec.sh

--- EXEC/exec.sh
+++ EXEC/exec.sh
@@ -5,7 +5,4 @@ URL="http://localhost:8080/api/v1/register"
 HEADERS="-H 'Connection: keep-alive' -H 'Keep-Alive: timeout=5, max=100'"
 
 # pattern1
-curl -vvv -XPOST $HEADERS -H "Content-Type: application/json" -d  '{"username":"aaaa", "profile_link":"http://localhost:8080/api/v1/users"}' $URL
-
-# pattern2
-# curl -vvv http://localhost:8080?http://localhost:9000/admin
+curl -vvv -XPOST $HEADERS -H "Content-Type: application/json" -d  '{"username":"aaaa", "profile_link":"http://localhost:9000/api/v1/users"}' $URL
